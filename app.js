eab67e7a0c331cf90b797b5fc8f1e6e8e7eda12b
Biswa-bob <veerbiswa00@gmail.com>
2021-07-12 22:50:34 +0530

    version 1.1

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT, function () {
+app.get("/about.html", function (req, res) {
+  res.sendFile(__dirname + "/about.html");
+});
+app.get("/contact.html", function (req, res) {
+  res.sendFile(__dirname + "/contact.html");
+});
+
+app.listen(process.env.PORT || 3000, function () {
   console.log("Successfully Running on server 3000 !");
 });
